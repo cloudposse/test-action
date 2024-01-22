2124a7b7c13c88cc485cdedf83a72291553690e8
Igor Rodionov <goruha@gmail.com>
2024-01-22 22:05:32 +0100

    Added plan

--- src/action.js
+++ src/action.js
@@ -17,7 +17,7 @@ const runAction = async (octokit, context, parameters) => {
     ref: sha
   });
 
-  console.log(github);
+  console.log(process.env);
 
   suites.data.check_runs
     .filter( item => { return item.name == "Plan (plat-sandbox) / plat-ue2-sandbox-test-drift-04"})
