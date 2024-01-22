0cd81a911e377d9257c1a4541afa3525d0040ef3
Igor Rodionov <goruha@gmail.com>
2024-01-22 20:10:58 +0100

    Added plan

---
+++ src/post.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {runAction} = require("./action");
+
+
+try {
+    // Get params
+    const token = core.getInput('token');
+    const sha = core.getInput('sha');
+
+    // Get octokit
+    const octokit = github.getOctokit(token);
+
+    runAction(octokit, github.context, {
+        sha,
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
