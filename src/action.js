30a9116e40e2d2afeab76a0efdc45a6bc58e1523
Igor Rodionov <goruha@gmail.com>
2024-01-22 19:15:46 +0100

    Added plan

--- src/action.js
+++ src/action.js
@@ -8,6 +8,21 @@ const {readFileSync} = require("fs");
  * @param {Object} parameters
  */
 const runAction = async (octokit, context, parameters) => {
+  const {
+    sha = "",
+  } = parameters;
+
+  // Run action
+  const suites = await octokit.rest.checks.listForRef({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: sha
+  });
+
+  console.log(suites);
+
+  suites.data.check_runs.forEach(item => { console.log(item) });
+
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {runAction} = require("../../atmos/terraform/drift-detection/src/action");
 
 try {
     // Get params
@@ -9,16 +10,9 @@ try {
     // Get octokit
     const octokit = github.getOctokit(token);
 
-    // Run action
-    const suites = await octokit.rest.checks.listForRef({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        ref: sha
+    runAction(octokit, github.context, {
+        sha,
     });
-
-    console.log(suites);
-
-    suites.data.check_runs.forEach(item => { console.log(item) });
 } catch (error) {
     core.setFailed(error.message);
 }
